0597f04ce8a47bf7736b51568ad4174bc8b66469
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-01-29 17:21:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ var katzDeliLine = [];
 //function takeANumber adds names to the katzDeliLine list
 
 function takeANumber(inLine, nextInLine) {
-	
-	
+
+
 	inLine.push(nextInLine);
 
 	var welcome = "Welcome, " + nextInLine + ". You are number " + inLine.length + " in line.";
-	
+
 	console.log(welcome);
 	return welcome;
 }
-
